fdfb141cf1ff34e95483e1010e3c762e337a84c2
fatimachubb <fatima.cruz2@chubb.com>
2023-05-02 14:47:09 -0600

    master

---
+++ HRV2/Controllers/Evaluar.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRV2.Controllers
+{
+    public class Evaluar : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
+
+
